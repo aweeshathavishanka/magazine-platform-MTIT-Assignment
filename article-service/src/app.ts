c4f3b0611bd61bab0595df85d13bd96fb9ee7a77
Dinitha Fernando <info@dinitha.me>
2026-03-26 19:21:42 +0530

    feat(article): wire Swagger UI to /api/v1/docs endpoint

--- article-service/src/app.ts
+++ article-service/src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import { notFoundHandler } from "./common/middlewares/notFoundHandler";
 import { router } from "./routes";
@@ -11,6 +13,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/v1", router);
 
